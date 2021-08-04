41468360ec774d64ca05805836922e24a2eac1b4
Aseer Intiser <aseerniloy@gmail.com>
2021-08-04 16:08:18 +0600

    update app.js

--- Fest2021/routes/MathOlympiad.routes.js
+++ Fest2021/routes/MathOlympiad.routes.js
@@ -3,7 +3,7 @@ const router=express.Router()
 
 const {ensureAuthenticated,addUserData} = require('../middlewares/auth.middleware');
 
-const {getMO,postMO,getMOList,deleteMO} = require('../controllers/mathOlympiad.controllers');
+const {getMO,postMO,getMOList,deleteMO} = require('../controllers/mathOlympiad.controller');
 
 router.get('/register', ensureAuthenticated, addUserData, getMO);
 router.post('/register', ensureAuthenticated, addUserData, postMO);
